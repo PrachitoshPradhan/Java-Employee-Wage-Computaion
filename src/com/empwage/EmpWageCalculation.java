b87f05f73a5f8dcee0d8a6be9895b717a48a14fd
PrachitoshPradhan <prachitoshpp.pradhans@gmail.com>
2022-08-26 01:05:20 +0530

    ADDED: Monthly Wage Calculation

--- src/com/empwage/EmpWageCalculation.java
+++ src/com/empwage/EmpWageCalculation.java
@@ -1,18 +1,40 @@
 package com.empwage;
 
 public class EmpWageCalculation {
-	static int isPresent = 1;
+	static final int isPresent = 1;
+	static final int halfDay =2;
+	static int WAGE_PER_HOUR = 20;
+	static int FULL_DAY = 8;
+	static int HALF_DAY_HOUR = 4;
+	static int noOfWorkingDays = 20;
+	
 	public static void main(String[] args) {
        System.out.println("Welcome to employee wage computaion programme.");
-       double attendance = Math.floor(Math.random()*10)%2;
-       if(attendance == isPresent)
+       int attendance;
+       int empWage=0;
+       int totalWage=0;
+       for(int day =1; day<=noOfWorkingDays;day++)
        {
-    	   System.out.println("Employee is present.");
-       }
-       else
+    	   attendance= (int)(Math.floor(Math.random()*10)%3);
+       switch (attendance)
        {
-    	   System.out.println("Employee is absent.");
+       case isPresent:
+    	   System.out.println("Employee is present today.");
+    	   empWage= WAGE_PER_HOUR * FULL_DAY;
+    	   break;
+    	   
+       case halfDay:
+    	   System.out.println("Employee has taken halfday.");
+    	   empWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+    	   break;
+    	   
+       default:
+    	   System.out.println("Employee is absent");
+    	   empWage = 0;
        }
+       totalWage = totalWage + empWage;
+	}
+       System.out.println("Employee total months wage is: " +totalWage);
 	}
 
 }
