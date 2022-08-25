aae16469434ab38a7f824eb7cce60aa805c86a2d
PrachitoshPradhan <prachitoshpp.pradhans@gmail.com>
2022-08-26 00:14:21 +0530

    ADDED: Daily Wage of Employee

--- src/com/empwage/EmpWageCalculation.java
+++ src/com/empwage/EmpWageCalculation.java
@@ -2,17 +2,24 @@ package com.empwage;
 
 public class EmpWageCalculation {
 	static int isPresent = 1;
+	static int WAGE_PER_HOUR = 20;
+	static int FULL_DAY = 8;
+	
 	public static void main(String[] args) {
        System.out.println("Welcome to employee wage computaion programme.");
        double attendance = Math.floor(Math.random()*10)%2;
+       int empWage;
        if(attendance == isPresent)
        {
-    	   System.out.println("Employee is present.");
+    	   System.out.println("Employee is present today.");
+    	   empWage= WAGE_PER_HOUR * FULL_DAY;
        }
        else
        {
-    	   System.out.println("Employee is absent.");
+    	   System.out.println("Employee is absent today.");
+    	   empWage = 0;
        }
+       System.out.println("Employee Wage is: " +empWage);
 	}
 
 }
