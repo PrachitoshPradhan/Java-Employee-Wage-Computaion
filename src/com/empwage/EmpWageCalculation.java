66d97ea497f5e370ce38b1bb82a59732faadf1dd
PrachitoshPradhan <prachitoshpp.pradhans@gmail.com>
2022-08-26 09:36:26 +0530

    final commit

--- src/com/empwage/EmpWageCalculation.java
+++ src/com/empwage/EmpWageCalculation.java
@@ -7,24 +7,29 @@ public class EmpWageCalculation {
 	static int FULL_DAY = 8;
 	static int HALF_DAY_HOUR = 4;
 	static int noOfWorkingDays = 20;
+	static int workingHourLimit= 100;
 	
 	public static void main(String[] args) {
        System.out.println("Welcome to employee wage computaion programme.");
        int attendance;
        int empWage=0;
        int totalWage=0;
-       for(int day =1; day<=noOfWorkingDays;day++)
+       int totalMonthHour=0;
+       int day=1;
+       while(day <= noOfWorkingDays && totalMonthHour <= workingHourLimit)
        {
     	   attendance= (int)(Math.floor(Math.random()*10)%3);
        switch (attendance)
        {
        case isPresent:
     	   System.out.println("Employee is present today.");
+    	   totalMonthHour = totalMonthHour + FULL_DAY;
     	   empWage= WAGE_PER_HOUR * FULL_DAY;
     	   break;
     	   
        case halfDay:
     	   System.out.println("Employee has taken halfday.");
+    	   totalMonthHour = totalMonthHour + HALF_DAY_HOUR;
     	   empWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
     	   break;
     	   
@@ -33,6 +38,7 @@ public class EmpWageCalculation {
     	   empWage = 0;
        }
        totalWage = totalWage + empWage;
+       day++;
 	}
        System.out.println("Employee total monthly wage is: " +totalWage);
 	}
